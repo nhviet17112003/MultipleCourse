8c8d6134210003ba87e9f8682dc864086037c4b3
khuuanhtuan123@gmail.com <khuuanhtuan123@gmail.com>
2025-02-11 09:54:34 +0700

    Fix loi rut tien

--- Backend/Controllers/CommentController.js
+++ Backend/Controllers/CommentController.js
@@ -88,6 +88,21 @@ exports.deleteCourseComment = async (req, res) => {
   }
 };
 
+//Show Course Comment
+exports.showCourseComment = async (req, res) => {
+  try {
+    const course = await Course.findById(req.params.courseId);
+    if (!course) {
+      return res.status(404).json({ message: "Course not found" });
+    }
+
+    res.status(200).json({ comments: course.comments });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
 // Comment for Lesson
 
 // Create Comment for Lesson
@@ -172,6 +187,21 @@ exports.deleteLessonComment = async (req, res) => {
     await lesson.save();
 
     res.status(200).json({ message: "Comment deleted successfully" });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+//Show Lesson Comment
+exports.showLessonComment = async (req, res) => {
+  try {
+    const lesson = await Lesson.findById(req.params.lessonId);
+    if (!lesson) {
+      return res.status(404).json({ message: "Lesson not found" });
+    }
+
+    res.status(200).json({ comments: lesson.comments });
   } catch (err) {
     console.log(err);
     res.status(500).json({ message: "Internal Server Error" });
