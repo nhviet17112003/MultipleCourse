e0480f17b097a4508560bf997b4402f06cd0efb2
datnguyen12032003 <danptce171966@fpt.edu.vn>
2025-02-28 00:28:36 +0700

    Lấy student của 1 khóa học và tiến độ

--- Backend/Controllers/ProgressController.js
+++ Backend/Controllers/ProgressController.js
@@ -1,6 +1,8 @@
 const Progress = require("../Models/Progress");
 const Course = require("../Models/Courses");
 const Lesson = require("../Models/Lessons");
+const User = require("../Models/Users");
+const Order = require("../Models/Orders");
 
 // Create a new progress
 exports.createProgress = async (req, res) => {
@@ -106,6 +108,72 @@ exports.updateProgress = async (req, res) => {
     progress.lesson = req.body.lesson;
     await progress.save();
     res.status(200).json(progress);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+//Show students progresses of course
+exports.getProgressByCourse = async (req, res) => {
+  try {
+    const course_id = req.params.course_id;
+    const course = await Course.findById(course_id);
+    if (!course) {
+      return res.status(404).json({ message: "Course not found" });
+    }
+    //Nếu trong đã mua khóa học nhưng chưa có progress thì in ra chưa enroll
+    //Nếu đã có progress thì in ra progress theo phần trăm dựa theo số khóa học đã hoàn thành
+
+    const order_items = await Order.find({
+      "order_items.course": course_id,
+    });
+    if (!order_items) {
+      return res.status(404).json({ message: "No student enrolled" });
+    }
+
+    const progresses = await Progress.find({ course_id: course_id });
+    if (!progresses) {
+      return res.status(404).json({ message: "No progress found" });
+    }
+
+    const students = [];
+
+    //Nếu trong order_items có course_id thì kiểm tra xem đã có progress chưa
+    //Nếu chưa có thì in ra chưa enroll
+    //Nếu có thì in ra progress theo phần trăm dựa theo số khóa học đã hoàn thành
+
+    for (let i = 0; i < order_items.length; i++) {
+      let student = await User.findById(order_items[i].user).select(
+        "_id fullname avatar"
+      );
+      let progress = progresses.find(
+        (progress) => progress.student_id.toString() === student._id.toString()
+      );
+      if (!progress) {
+        students.push({
+          student: student,
+          status: "Not Enrolled",
+          percent: 0,
+        });
+      } else {
+        let lesson = progress.lesson;
+        let totalLesson = lesson.length;
+        let completedLesson = 0;
+        for (let j = 0; j < lesson.length; j++) {
+          if (lesson[j].status === "Completed") {
+            completedLesson++;
+          }
+        }
+        let percent = (completedLesson / totalLesson) * 100;
+        students.push({
+          student: student,
+          status: "Enrolled",
+          percent: percent,
+        });
+      }
+    }
+    res.status(200).json(students);
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: "Internal server error" });
