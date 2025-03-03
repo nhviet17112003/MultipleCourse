eb8c3a16f5b6fbedfcaca86ef419ae1b438ecb88
HoangTrongBETI <hoangtrongbeti66@gmail.com>
2025-03-03 13:28:39 +0700

    loading avt

--- frontend/src/components/students/UpdateProfile.js
+++ frontend/src/components/students/UpdateProfile.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 const UpdateProfile = () => {
   
   const [formData, setFormData] = useState({
