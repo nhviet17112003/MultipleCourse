c69937e1c37ddb3609022fee38708f52734ee91b
datnguyen12032003 <144867683+datnguyen12032003@users.noreply.github.com>
2025-04-13 14:28:14 +0700

    Update axiosConfig.js

--- frontend/src/axiosConfig.js
+++ frontend/src/axiosConfig.js
@@ -1,7 +1,7 @@
 // src/axiosConfig.js
 import axios from 'axios';
 const instance = axios.create({
-    baseURL: "http://localhost:3000/",    //backend
+    baseURL: "https://multicourse.onrender.com",    //backend
 
     headers: {
       "Content-Type": "application/json",
