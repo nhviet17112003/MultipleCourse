150b2a002858eaffaae4b127c6cf8822b7bb1678
datnguyen12032003 <144867683+datnguyen12032003@users.noreply.github.com>
2025-04-13 16:08:42 +0700

    Update Cors.js

--- Backend/Loaders/Cors.js
+++ Backend/Loaders/Cors.js
@@ -11,7 +11,7 @@ const whitelist = [
 const corsOptionsDelegate = (req, callback) => {
   let corsOptions;
   if (whitelist.indexOf(req.header("Origin")) !== -1) {
-    corsOptions = { origin: true };
+    corsOptions = { origin: true, credentials: true, };
   } else {
     corsOptions = { origin: false };
   }
